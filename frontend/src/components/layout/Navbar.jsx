9b56d4553311a055447cb3f5346798e4dfb60fce
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-01 20:03:05 -0500

    frontend folder

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,43 @@
+import {Link, useNavigate} from 'react-router-dom'
+import Logo from './Logo'
+import { useSelector, useDispatch } from 'react-redux'
+import {logout, reset} from '../../features/auth/authSlice'
+const Navbar = () => {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const {user} = useSelector((state) => state.auth)
+
+  const onLogout = ()=>{
+    dispatch (logout())
+    dispatch(reset())
+    navigate('/')
+  }
+  return (
+    
+<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <div className="container flex flex-wrap items-center justify-between mx-auto">
+    <Link to="/" className="flex items-center">
+      <Logo className="h-6 mr-3 sm:h-9"/>
+      <div className="flex flex-col">
+        <span className=" text-xl font-semibold whitespace-nowrap dark:text-white">ORTA</span>
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Bılımge qūştar bol</span>
+        </div>
+    </Link>
+    {user ? (
+          <li>
+            <button className='btn text-white' onClick={onLogout}>
+               Logout
+            </button>
+          </li>
+        ) : (
+    <div className="w-full md:w-auto flex flex-row justify-between" id="navbar-default">
+        <Link to='/login' className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">LOG IN</Link>
+        <Link to='/register' className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">SIGN UP</Link>
+    </div>)}
+
+  </div>
+</nav>
+  )
+}
+export default Navbar
