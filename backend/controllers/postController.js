306d738171b110e82147d8d74ceed5a982d392b0
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-18 16:25:15 -0500

    added post route and post controller

---
+++ backend/controllers/postController.js
@@ -0,0 +1,109 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel')
+const Post = require('../models/postModel')
+
+// @desc Get user posts
+// @route GET /api/posts
+// @access Private
+const getPosts = asyncHandler(async (req,res)=>{
+
+    const posts = await Post.find({user: req.user.id});
+
+    res.status(200).json(posts)
+
+})
+
+// @desc Get user post
+// @route GET /api/posts/:id
+// @access Private
+const getPost = asyncHandler(async (req,res)=>{
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+        res.status(404)
+        throw new Error('Post not found')
+    } 
+    if (post.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not Authorized')
+      }
+    res.status(200).json(post)
+})
+
+// @desc Create new post
+// @route POST /api/posts
+// @access Private
+const createPost = asyncHandler(async (req,res)=>{
+    const {title, description, text, tags} = req.body
+
+    if(!title || !description || !text){
+        res.status(400)
+        throw new Error('Please make sure your post has a title, description and the body text')
+    }
+
+    const post = await Post.create({
+        user: req.user.id,
+        title ,
+        description,
+        text,
+        comments: [],
+        tags: []
+    })
+    if(post){
+        res.status(201).json(post)
+    } 
+})
+
+// @desc Delete user post
+// @route delete /api/posts/:id
+// @access Private
+const deletePost = asyncHandler(async (req,res)=>{
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      res.status(404)
+      throw new Error('post not found')
+    }
+  
+    if (post.user.toString() !== req.user.id) {
+      res.status(401)
+      throw new Error('Not Authorized')
+    }
+
+    await post.remove()
+    res.status(200).json({success: true})
+})
+
+// @desc Update user post
+// @route Update /api/posts/:id
+// @access Private
+const updatePost = asyncHandler(async (req,res)=>{
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      res.status(404)
+      throw new Error('post not found')
+    }
+  
+    if (post.user.toString() !== req.user.id) {
+      res.status(401)
+      throw new Error('Not Authorized')
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    )
+    res.status(200).json(updatedPost)
+})
+
+
+module.exports = {
+    getPost,
+    getPosts,
+    createPost,
+    deletePost,
+    updatePost
+    
+}
---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const {getPost, getPosts,createPost, deletePost, updatePost} = require('../controllers/postController')
+const {protect} = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getPosts).post(protect, createPost)
+router.route('/:id').get(protect, getPost).delete(protect, deletePost).put(protect, updatePost)
+
+
+module.exports = router;
