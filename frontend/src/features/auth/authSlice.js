d8f24627e6368fa239cd6cd76fa09578dafd8d8e
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-21 15:39:35 -0500

    added extractErrorMessage func to avoid repetition

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,15 +1,12 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import {createSlice, createAsyncThunk, createAction} from '@reduxjs/toolkit'
 import authService from './authService'
-
+import {extractErrorMessage} from '../../utils'
 
 const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
     user: user? user:null,
-    isError: false,
-    isSuccess: false,
     isLoading: false,
-    message: ''
 }
 
 export const register = createAsyncThunk('auth/register', async(user, thunkAPI)=>{
@@ -17,9 +14,7 @@ export const register = createAsyncThunk('auth/register', async(user, thunkAPI)=
         return await authService.register(user)
 
     } catch (error) {
-        const message = (error.response && error.response.data &&
-        error.response.data.message) || error.message || error.toString()
-        return thunkAPI.rejectWithValue(message)
+        return thunkAPI.rejectWithValue(error)
     }
 })
 
@@ -28,25 +23,21 @@ export const login = createAsyncThunk('auth/login', async(user, thunkAPI)=>{
         return await authService.login(user)
 
     } catch (error) {
-        const message = (error.response && error.response.data &&
-        error.response.data.message) || error.message || error.toString()
-        return thunkAPI.rejectWithValue(message)
+        return thunkAPI.rejectWithValue(error)
     }
 })
 
-export const logout = createAsyncThunk('auth/logout', async ()=>{
-    await authService.logout()
+export const logout = createAction('auth/logout', ()=>{
+    authService.logout()
+    return{}
 })
 
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        reset: (state)=>{
-            state.isLoading=false
-            state.isError =false
-            state.isSuccess = false
-            state.message=''
+        logout: (state)=>{
+            state.user = null
         }
     },
     extraReducers: (builder) =>{
@@ -55,35 +46,23 @@ export const authSlice = createSlice({
             state.isLoading = true
         })
         .addCase(register.fulfilled, (state, action)=>{
-            state.isLoading=false
-            state.isSuccess = true
             state.user = action.payload
+            state.isLoading =false
         })
 
-        .addCase(register.rejected, (state, action)=>{
+        .addCase(register.rejected, (state)=>{
             state.isLoading=false
-            state.isError = true
-            state.message = action.payload
-            state.user = null
         })
         .addCase(login.pending, (state)=>{
             state.isLoading = true
         })
         .addCase(login.fulfilled, (state, action)=>{
-            state.isLoading=false
-            state.isSuccess = true
             state.user = action.payload
+            state.isLoading=false
         })
 
         .addCase(login.rejected, (state, action)=>{
             state.isLoading=false
-            state.isError = true
-            state.message = action.payload
-            state.user = null
-        })
-        .addCase(logout.fulfilled, (state)=>{
-
-            state.user = null
         })
     }
 })
