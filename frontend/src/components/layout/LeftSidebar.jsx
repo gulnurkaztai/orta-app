6ead72efad92a89b7c2bc5b9535003d1914bf846
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-25 17:52:14 -0500

    styling

--- frontend/src/components/layout/LeftSidebar.jsx
+++ frontend/src/components/layout/LeftSidebar.jsx
@@ -2,7 +2,7 @@ const LeftSidebar = () => {
   return (
     <> 
     {/* needs to become a hamburger menu in the logo */}
-                <div className="mx-2 mt-2 w-3/12 hidden lg:block font-display bg-gray-800 rounded-2xl">
+                <div className="mx-2 w-3/12 hidden lg:block font-display bg-gray-800 rounded-2xl">
                     <div className="mt-10 px-8">
                         <h1 className=" text-xl font-bold text-gray-400 hover:text-gray-100">Jazbalar</h1>
                         <users-list></users-list>
