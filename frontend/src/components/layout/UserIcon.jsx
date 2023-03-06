e988b3b527cd50cbecbf5ea650f2044a84ce69f0
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-05 22:31:49 -0500

    styled the user icon

--- frontend/src/components/layout/UserIcon.jsx
+++ frontend/src/components/layout/UserIcon.jsx
@@ -19,7 +19,7 @@ const UserIcon = () => {
     }
 
   return (
-    <div className='flex flex-row w-32 items-center relative'>
+    <div className='flex flex-row w-32 items-center relative ml-10'>
         <FiMail className='basis-1/3 w-6 h-6'/>
         <MdOutlineNotificationsNone className='basis-1/3 w-6 h-6'/>
 
@@ -34,6 +34,9 @@ const UserIcon = () => {
 
     <div id="dropdown" className={`${userMenu? "block" : "hidden"} absolute right-0 top-10 bg-white  rounded-lg shadow w-32 dark:bg-gray-700`} >
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 font-display">
+        <li>
+            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Account</a>
+          </li>
           <li>
             <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
           </li>
